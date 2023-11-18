c71cc0b868adf569e77ab976c1ebce94dbf66e4a
Omarabdullah99 <omarabdullah917303@gmail.com>
2023-11-18 18:25:11 +0600

    Login-code

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express'
-import { signup, testUser } from '../controllers/userController.js'
+import { signin, signup, testUser } from '../controllers/userController.js'
 
 const router= express.Router()
 
 router.get("/",testUser)
 router.post('/signup', signup)
+router.post("/signin",signin)
 
 export default router
